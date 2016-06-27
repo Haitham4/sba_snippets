b76c7649f6872d87e85ca725ab0b0f0763ac2fe9
Haitham Fallatah <Haitham.Fallatah.1990@gmail.com>
2016-06-27 12:19:50 -0400

    Added search ability by Language & title + (Hopfully heroku ready)

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,12 @@
 class Snippet < ActiveRecord::Base
+
+  def search_data
+    {
+        title: title,
+        language: language.name
+    }
+  end
+
   belongs_to :user
   belongs_to :language
   has_many :comments, -> { order(updated_at: :desc) }
@@ -6,4 +14,10 @@ class Snippet < ActiveRecord::Base
   validates_presence_of :user, :language, :title, :code
 
   scope :latest, -> { order(updated_at: :desc) }
+
+  searchkick
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
 end
